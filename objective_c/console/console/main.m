bf26e3f5f8588e7d23e95c4e81d783b43b67b9de
syutaro1979 <seafield1979@gmail.com>
2014-08-03 01:08:35 +0900

    NSDataのサンプル追加

--- objective_c/console/console/main.m
+++ objective_c/console/console/main.m
@@ -14,6 +14,7 @@
 #import "arrayTest.h"
 #import "dictionaryTest.h"
 #import "blockTest.h"
+#import "dataTest.h"
 
 void testSharedObject();
 void testNSData();
@@ -91,19 +92,20 @@ int main(int argc, const char * argv[])
 				blockTest *bt = [[blockTest alloc]init];
 				[bt test1];
 			}
+			else if(0 == strcmp((const char*)cmd_buf, "data1")){
+				dataTest *dt = [[dataTest alloc]init];
+				[dt test1];
+			}
+			else if(0 == strcmp((const char*)cmd_buf, "data2")){
+				dataTest *dt = [[dataTest alloc]init];
+				[dt test2];
+			}
 			else{
 				NSLog(@"exit");
 				break;
 			}
 
 		}
-
-//		testSharedObject();
-//		testNSData();
-//
-//		userDefaultTest *udt = [[userDefaultTest alloc]init];
-//		[udt test1];
-
 	}
     return 0;
 }
@@ -119,37 +121,5 @@ void testSharedObject()
 	sharedObject *so2 = [sharedObject getNewObj];
 	NSLog(@"sharedObject %@", so2);
 
-}
-
-/**
- * NSDataのテスト
- */
-void testNSData()
-{
-	NSString *homeDir = NSHomeDirectory();
-	NSLog(@"%@", homeDir);
-
-	// ファイルからNSDataを作成
-	// 事前準備として プロジェクトの[Build Phases] - [Copy Files] に
-	// Destination - Absolute Path
-	// Path - $(Home)
-	// を設定する。こうすることでホームディレクトリにCopy Filesのファイルがコピーされる
-	NSString *path = [NSString stringWithFormat:@"%@/test1.txt", homeDir];
-	NSData *data = [[NSData alloc]initWithContentsOfFile:path];
-
-	// ファイルにNSDataを書き出す
-	NSData *data2  = [[NSData alloc]initWithBytes:[@"syutaro" UTF8String] length:[@"syutaro" length]];
-	NSString *writePath = [NSString stringWithFormat:@"%@/test2.txt", homeDir];
-	[data2 writeToFile:writePath atomically:YES];
-
-	// 表示(16進数)
-	NSLog(@"NSData %@", [data description]);
-
-	// 文字列で表示
-	NSLog(@"NSData(str) %@", [data description2]);
-
-	// データ長
-	NSUInteger len = [data length];
-	NSLog(@"NSData(length) %lu", len );
 }
 

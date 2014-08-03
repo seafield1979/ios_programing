cdcb6f6ef8a33b3c0850bc0ebc5c55b00f7b2f20
syutaro1979 <seafield1979@gmail.com>
2014-08-04 01:31:46 +0900

    iOSサンプルのプロジェクトを追加
    今後、これらのプロジェクトにサンプルコードを追加する

---
+++ objective_c/ScrollViewTest/ScrollViewTest/DataViewController.h
@@ -0,0 +1,16 @@
+//
+//  DataViewController.h
+//  ScrollViewTest
+//
+//  Created by 海野 秀祐 on 2014/08/04.
+//  Copyright (c) 2014年 海野 秀祐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end

d61f9759d90e9dcaf8ebb4e90f7c2fcf96e0b32c
Shusuke Unno <unno_shunsuke_gn@cyberagent.co.jp>
2014-08-07 19:59:31 +0900

    チュートリアル作成中
    チュートリアルの進行をステート管理メソッド(stateFunc)で運用する

--- objective_c/AOTutorialSample/AOTutorialSample/ViewController.m
+++ objective_c/AOTutorialSample/AOTutorialSample/ViewController.m
@@ -8,14 +8,26 @@
 
 #import "ViewController.h"
 
+typedef enum{
+	STATE_INIT = 0,			// 初期状態
+	STATE_TELOP_APP1,		//「この画像、あなたならどうボケる？？」の表示１ 0%->110%->100%
+	STATE_TELOP_WAIT,		//「この画像、あなたならどうボケる？？」の表示中
+	STATE_BOKE_APPEAR,		// ボケのテキスト出現中
+	STATE_BOKE_WAIT,		// ボケテキスト表示
+	STATE_ZABU_APPEAR,		// 座布団出現中
+	STATE_ZABU_WAIT,		// 座布団を３回押すまで
+	STATE_PAGE_MOVING,		// ページ遷移
+	STATE_PAGE_WAIT,		// ページ遷移後の待ち
+	STATE_BUTTON_APPEAR,	// ログインボタン表示中
+	STATE_BUTTON_WAIT		// ボタンが押されるまで
+}TutorialState;
 
 @interface ViewController ()
 {
-//	UIView *_page1View;
-//	UIView *_page2View;
-//	UIView *_bokeBGView;
-//	UIImageView *_bokeImgView;
-//	NSMutableArray *_imageViewList;
+	int state;
+	int timerCount;
+	NSTimer *_tm;
+	CGRect screenRect;
 }
 
 @end
@@ -27,6 +39,8 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+		state = STATE_INIT;
+		timerCount = 0;
     }
     return self;
 }
@@ -36,82 +50,127 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 	
-	CGRect rect = [[UIScreen mainScreen] bounds];
+	screenRect = [[UIScreen mainScreen] bounds];
 	
-	_page1View.frame = CGRectMake(0, 20, rect.size.width, rect.size.height);
+	_page1View.frame = CGRectMake(0, 20, screenRect.size.width, screenRect.size.height);
 	[self.view addSubview:_page1View];
 	
-#if 0
-	// UIViewを作成
-	_page1View = [[UIView alloc]initWithFrame:CGRectMake(0, 20, screen_size.size.width, screen_size.size.height - 20)];
-	_page2View = [[UIView alloc]initWithFrame:CGRectMake(0, 0, screen_size.size.width, screen_size.size.height)];
-	// ボケエリアのView
-	_bokeBGView = [[UIView alloc]initWithFrame:CGRectMake(18, 35, 284, 350)];
-	_bokeBGView.backgroundColor = [UIColor blackColor];
-	_bokeBGView.layer.cornerRadius = 5;
-	_bokeBGView.clipsToBounds = true;
+	// いろいろ非表示
+	_telop1View.hidden = YES;
+	_telop2View.hidden = YES;
 	
-	[self.view addSubview:_page1View];
-	[_page1View addSubview:_bokeBGView];
 	
-
-//	UIImageView *bokeImgView;
-//	NSArray *imageViewList;
-//	NSArray *zabutonImgList;
-
-	// UIImageViewを作成
-	NSArray *imgNameList = @[@{@"name" : @"image/boke_obi.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/boke_photo.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/boke_text_cardboard-568.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/boke_text_hatena.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/boke_text_how.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/boke_text_tapzabuton.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/boke_yubi_big.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_0", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_1", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_2", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_3", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_star_small.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_star_big.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_star_left.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_star_right.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_text_0.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_text_1.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/zabuton_text_2.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/login_logo_pigg-568.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/login_text_caution.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/login_icon_sns.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/login_button_start_on-568.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/login_button_start_568.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/boke_obi.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/boke_obi.png", @"x" : @"10", @"y" : @"20"},\
-							 @{@"name" : @"image/boke_obi.png", @"x" : @"10", @"y" : @"20"}];
-	_imageViewList = [[NSMutableArray alloc]init];
-	for(NSString *name in imgNameList){
-		UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:name]];
-		[_imageViewList addObject:imageView];
-	}
-	// 初期表自分のレイアウトを作成する
-	[_page1View addSubview: [_imageViewList objectAtIndex:IMG_TOP_BAR]];
-	
-	UIImageView *imageView = (UIImageView*)[_imageViewList objectAtIndex:IMG_BOKE];
-	CGRect rect = imageView.frame;
-	imageView.frame = CGRectMake(25, 20, rect.size.width, rect.size.height);
-	[_bokeBGView addSubview: imageView];
-#endif 
-//	IMG_BOKE,
-//	IMG_BOKE_TXT_1,		// ???
-//	IMG_BOKE_TXT_2,		// ダンボールだけ〜
-//	IMG_TELOP_1,		// この画像、あなたならどうボケる？？
-//	IMG_TELOP_2,		// 座布団をタップしてみよう
-	 
+	_tm =
+	[NSTimer
+		scheduledTimerWithTimeInterval:2.0f    // タイマーを発生させる間隔(秒)
+		target:self                            // タイマー発生時に呼び出すメソッドがあるターゲット
+		selector:@selector(stateFunc)        // タイマー発生時に呼び出すメソッド
+		userInfo:nil                           // selectorで呼び出すメソッドに渡す情報(NSDictionary)
+		repeats:YES                            // タイマーの実行を繰り返すかどうかの指定 （YES：繰り返す　NO：１回のみ）
+	 ];
+	state = STATE_TELOP_APP1;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+
 }
+
+// 各種状態で行う処理
+-(void)stateFunc{
+	NSLog(@"stateFunc %d", state);
+	switch(state){
+		case STATE_TELOP_APP1:		//「この画像、あなたならどうボケる？？」の表示１ 0%->110%
+			{
+				state++;
+				_telop1View.hidden = NO;
+				CGRect rect = _telop1View.frame;
+				CGRect rect2;
+				rect2 = _telop1View.frame = CGRectMake(rect.origin.x + rect.size.width / 2,
+											   rect.origin.y + rect.size.height / 2,
+											   0, 0);
+				[UIView animateWithDuration:0.7f
+					delay:0.0
+//					options:UIViewAnimationOptionCurveEaseIn
+ 					options:UIViewAnimationOptionCurveLinear
+					animations:^(void){
+						_telop1View.frame = CGRectMake(rect2.origin.x - rect.size.width * 0.55,
+												   rect2.origin.y - rect.size.height * 0.55,
+												   rect.size.width * 1.1, rect.size.height * 1.1);
+					}
+				 completion:^(BOOL finished){
+					 [UIView animateWithDuration:0.2f
+										   delay: 0.0f
+//										 options:UIViewAnimationOptionCurveEaseOut
+										 options:UIViewAnimationOptionCurveLinear
+						animations:^(void){
+						 _telop1View.frame = CGRectMake(rect2.origin.x - rect.size.width * 0.5,
+														rect2.origin.y - rect.size.height * 0.5,
+														rect.size.width, rect.size.height);
+					 }
+						  completion:^(BOOL finished){
+							  [self stateFunc];
+						  }];
+				 }];
+			}
+			break;
+		case STATE_TELOP_WAIT:		//「この画像、あなたならどうボケる？？」の表示中
+			state++;
+			{
+				CGRect text2OriginRect = _bokeText2View.frame;
+				_bokeText2View.frame = CGRectMake(text2OriginRect.origin.x + text2OriginRect.size.width / 2,
+											  text2OriginRect.origin.y + text2OriginRect.size.height / 2,
+											  0, 0);
+				_bokeText2View.hidden = NO;
+				
+				[UIView animateWithDuration:1.0f
+								animations:^(void){
+									// ???を縮小
+									CGRect rect = _bokeText1View.frame;
+									_bokeText1View.frame = CGRectMake(rect.origin.x + rect.size.width / 2,
+																	rect.origin.y + rect.size.height / 2,
+																	  0, 0);
+									// ボケを拡大
+									_bokeText2View.frame = text2OriginRect;
+								}
+								completion:^(BOOL finished){
+									[self stateFunc];
+								}];
+			}
+			break;
+		case STATE_BOKE_APPEAR:		// ボケのテキスト出現中
+			state++;
+			break;
+		case STATE_BOKE_WAIT:		// ボケテキスト表示
+			state++;
+			break;
+		case STATE_ZABU_APPEAR:		// 座布団出現中
+			state++;
+			break;
+		case STATE_ZABU_WAIT:		// 座布団を３回押すまで
+			state++;
+			break;
+		case STATE_PAGE_MOVING:		// ページ遷移
+			state++;
+			break;
+		case STATE_PAGE_WAIT:		// ページ遷移後の待ち
+			state++;
+			break;
+		case STATE_BUTTON_APPEAR:	// ログインボタン表示中
+			state++;
+			break;
+		case STATE_BUTTON_WAIT:		// ボタンが押されるまで
+			state++;
+			break;
+	}
+	
+	
+	// 停止
+	[_tm invalidate];
+}
+
 
 - (IBAction)pushButton1:(id)sender {
 	NSLog(@"pushButton1");

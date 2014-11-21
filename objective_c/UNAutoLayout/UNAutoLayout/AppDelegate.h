3191796f2bd07db47c6a64ad3e0ac13113541397
Shusuke Unno <unno_shunsuke_gn@cyberagent.co.jp>
2014-11-21 19:23:06 +0900

    AutoLayoutテストプロジェクト追加

---
+++ objective_c/UNAutoLayout/UNAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UNAutoLayout
+//
+//  Created by B02681 on 2014/11/21.
+//  Copyright (c) 2014年 B02681. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objective_c/UNAutoLayout/UNAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UNAutoLayout
+//
+//  Created by B02681 on 2014/11/21.
+//  Copyright (c) 2014年 B02681. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

4a56facda9d874a64f0d245677908d3a97652ba7
shusuke unno <seafield1979@yahoo.co.jp>
2014-12-19 00:53:28 +0900

    ScrollViewテスト

--- objective_c/UNTextViewTest2/UNTextViewTest2/ViewController.m
+++ objective_c/UNTextViewTest2/UNTextViewTest2/ViewController.m
@@ -10,18 +10,49 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIView *contentView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self initScrollView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Private method
+/**
+ * スクロールビューを初期化
+ * 4ページ分のコンテンツをスクロールする
+ */
+-(void)initScrollView
+{
+//    self.scrollView = [[UIScrollView alloc]initWithFrame: CGRectMake(0, 0, 320, 480)];
+    self.scrollView.contentSize = CGSizeMake(320, 1000);
+    self.scrollView.delegate = self;
+//    
+//    // viewにScrollViewを追加
+//    [self.view addSubview:self.scrollView];
+//    
+//    // ScrollViewに表示するViewを生成
+//    self.contentView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 1000)];
+//    self.contentView.backgroundColor = [UIColor redColor];
+//    
+//    // ScrollViewにviewを追加
+//    [self.scrollView addSubview:self.contentView];
+    
+    
+}
+
+
+
+
 
 @end

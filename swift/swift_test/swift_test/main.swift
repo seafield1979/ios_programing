922193c3f4d705580a5eba64c3a6e6f0f569688c
Shusuke Unno <unno_shunsuke_gn@cyberagent.co.jp>
2015-01-23 11:22:57 +0900

    swiftテスト用のプロジェクト追加

---
+++ swift/swift_test/swift_test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift_test
+//
+//  Created by B02681 on 2015/01/23.
+//  Copyright (c) 2015年 B02681. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+

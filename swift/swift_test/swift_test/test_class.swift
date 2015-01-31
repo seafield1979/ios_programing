23020cbdcaaaf299b56b82436efc99b0c267a068
shusuke unno <seafield1979@gmail.com>
2015-01-31 23:05:07 +0900

    swiftサンプル
    ファイルを追加

---
+++ swift/swift_test/swift_test/test_class.swift
@@ -0,0 +1,9 @@
+//
+//  test_class.swift
+//  swift_test
+//
+//  Created by UnnoShusuke on 2015/01/29.
+//  Copyright (c) 2015年 B02681. All rights reserved.
+//
+
+import Foundation
---
+++ swift/swift_test/swift_test/test_function.swift
@@ -0,0 +1,9 @@
+//
+//  test_function.swift
+//  swift_test
+//
+//  Created by UnnoShusuke on 2015/01/31.
+//  Copyright (c) 2015年 B02681. All rights reserved.
+//
+
+import Foundation

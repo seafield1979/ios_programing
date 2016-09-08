a3d964eb35db66b99f37c22b9f96b9e3d9ed079f
shusuke unno <seafield1979@gmail.com>
2016-09-08 11:35:29 +0900

    Nibを使ったViewの生成サンプル

--- swift/NibTest/NibTest/ViewController.swift
+++ swift/NibTest/NibTest/ViewController.swift
@@ -13,16 +13,35 @@ class ViewController: UIViewController {
     @IBAction func buttonTapped(sender: AnyObject) {
         print("hello mate")
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        self.loadTemplate()
+        loadTemplate()
+        loadCustomView()
     }
     
-    func loadTemplate(){
+    // MyViewを作成
+    func loadTemplate() {
         let myView = UINib(nibName: "MyView", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! UIView
+        let subview1 = myView.subviews[1] as UIView
+        myView.frame.size = CGSizeMake( subview1.frame.size.width, subview1.frame.size.height)
+        myView.backgroundColor = .blueColor()
         self.view.addSubview(myView)
+    }
+    
+    // CustomViewを作成
+    func loadCustomView()
+    {
+        let customView = CustomView()
+        customView.frame.origin = CGPointMake(0,150)
+        self.view.addSubview(customView)
+        
+        // テキストとボタンを押した時の処理を追加
+        let customView2 = CustomView(frame: CGRectMake(0,250,0,0))  // サイズは使われないので0
+        self.view.addSubview(customView2)
+        customView2.label1.text = "hoge"
+        customView2.button1.addTarget(self, action: #selector(self.buttonTapped(_:)), forControlEvents: .TouchUpInside)
     }
 }
 

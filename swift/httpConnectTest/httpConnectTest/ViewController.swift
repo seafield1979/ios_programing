0eb6b9bc4eb1b9546a1bc2c7d60f7f107044214f
shusuke unno <seafield1979@gmail.com>
2016-09-10 18:07:49 +0900

    HTTP通信のサンプル
    同期型のhttp通信処理

--- swift/httpConnectTest/httpConnectTest/ViewController.swift
+++ swift/httpConnectTest/httpConnectTest/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, NSURLSessionTaskDelegate, NSURLSessionDataDelegate {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, NSURLSessionTaskDelegate {
 
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var picker1: UIPickerView!
@@ -18,18 +18,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         "hoge1","hoge2", "hoge3", "hoge4"
     ]
     
+    let testURL = "http://sunsunsoft.com/test/hoge1.php"
+    
     var responseData : NSMutableData! = nil
     
     func button1Tapped(button : UIButton) {
-        print(picker1.selectedRowInComponent(0))
-        
         let testNo = picker1.selectedRowInComponent(0)
         
         switch (testNo) {
         case 0:
             test1()
         case 1:
-            print("")
+            test2()
         case 2:
             print("")
         case 3:
@@ -40,9 +40,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     // MARK: test
+    // 非同期型http通信
+    // 必要なタイミングでdelegateメソッドが呼ばれれる
     func test1() {
         // 送信したいURLを作成し、Requestを作成します。
-        let url = NSURL(string:"http://sunsunsoft.com/test/hoge1.php")
+        let url = NSURL(string:testURL)
         let request = NSURLRequest(URL: url!)
         
         // NSURLSessionConfigurationでhttp通信を接続
@@ -51,6 +53,48 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             
         let task:NSURLSessionDataTask = session.dataTaskWithRequest(request)
         task.resume()
+    }
+    
+    // 同期型http通信
+    // 通信が終わるまで関数から出てこない
+    func test2() {
+        // create the url-request
+        let urlString = testURL
+        let request = NSMutableURLRequest(URL: NSURL(string: urlString)!)
+        
+        // set the method(HTTP-GET)
+        request.HTTPMethod = "GET"
+        
+        // use NSURLSessionDataTask
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { data, response, error in
+            if (error == nil)
+            {
+                // サブスレッドで textView1にアクセスすると落ちるのでメインスレッドで処理する
+                self.dispatch_async_main {
+                    let result = NSString(data: data!, encoding: NSUTF8StringEncoding)!
+                    self.textView1.text = result as String
+                }
+            } else {
+                print(error)
+            }
+        })
+        task.resume()
+    }
+    
+    // メインスレッド
+    func dispatch_async_main(block: () -> ()) {
+        dispatch_async(dispatch_get_main_queue(), block)
+    }
+    // バックグラウンド
+    func dispatch_async_global(block: () -> ()) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
+    }
+    
+    //レスポンス取得後の処理
+    func getHttp(res:NSURLResponse?,data:NSData?,error:NSError?)
+    {
+        let response:NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
+        print(response)
     }
     
     func saveDataToFile(data : NSData, fileName : String) {
@@ -78,7 +122,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
     }
     
-    // MARK: NSURLSessionDelegate
+    // MARK: NSURLSessionTaskDelegate
     /**
      * HTTPリクエストのデリゲートメソッド(データ受け取り初期処理)
      */
@@ -147,7 +191,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     // pickerが選択された際に呼ばれるデリゲートメソッド.
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
     {
-        print("row: \(row)\n value: \(pickerTitles[row])")
+        //print("row: \(row)\n value: \(pickerTitles[row])")
     }
 }
 

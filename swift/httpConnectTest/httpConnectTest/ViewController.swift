14a7a8c62b3d416cf0c6b7e4aeb61dd5f3577eda
shusuke unno <seafield1979@gmail.com>
2016-09-12 14:21:19 +0900

    AFNetworkingサンプル追加
    GET,POST送信
    プレーンテキスト受信
    テキスト形式のJSON受信
    JSONオブジェクト形式受信

--- swift/httpConnectTest/httpConnectTest/ViewController.swift
+++ swift/httpConnectTest/httpConnectTest/ViewController.swift
@@ -15,10 +15,20 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var textView1: UITextView!
     
     private let pickerTitles: NSArray = [
-        "hoge1","hoge2", "hoge3", "hoge4"
+        "http 同期",
+        "http 非同期",
+        "AFNetwork GET",
+        "AFNetwork POST",
+        "AFNetwork JSON",
+        "AFNetwork JSON2",
+        "AFNetwork JSON3"
     ]
     
-    let testURL = "http://sunsunsoft.com/test/hoge1.php"
+    let testGetURL = "http://sunsunsoft.com/test/ios/test_get.php"
+    let testPostURL = "http://sunsunsoft.com/test/ios/test_post.php"
+    let testJsonURL = "http://sunsunsoft.com/test/ios/test.json"
+    let testJsonURL2 = "http://sunsunsoft.com/test/ios/test_json.php"
+    let testJsonURL3 = "http://sunsunsoft.com/test/ios/test_json2.php"
     
     var responseData : NSMutableData! = nil
     
@@ -27,13 +37,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         switch (testNo) {
         case 0:
-            test1()
+            testSynchronous()
         case 1:
-            test2()
+            testAsynchronous()
         case 2:
-            test3()
+            testAFNetworkingGET()
         case 3:
-            print("")
+            testAFNetworkingPOST()
+        case 4:
+            testAFNetworkingJSON()
+        case 5:
+            testAFNetworkingJSON2()
+        case 6:
+            testAFNetworkingJSON3()
         default:
             break
         }
@@ -42,9 +58,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     // MARK: test
     // 非同期型http通信
     // 必要なタイミングでdelegateメソッドが呼ばれれる
-    func test1() {
+    func testSynchronous() {
         // 送信したいURLを作成し、Requestを作成します。
-        let url = NSURL(string:testURL)
+        let url = NSURL(string:testGetURL)
         let request = NSURLRequest(URL: url!)
         
         // NSURLSessionConfigurationでhttp通信を接続
@@ -57,9 +73,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     // 同期型http通信
     // 通信が終わるまで関数から出てこない
-    func test2() {
+    func testAsynchronous() {
         // create the url-request
-        let urlString = testURL
+        let urlString = testGetURL
         let request = NSMutableURLRequest(URL: NSURL(string: urlString)!)
         
         // set the method(HTTP-GET)
@@ -109,14 +125,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
     
-    // GETでプレーンテキストを取得
-    func test3() {
+    // GET送信
+    // プレーンテキスト取得
+    func testAFNetworkingGET() {
         
         let manager = AFHTTPRequestOperationManager()
         manager.responseSerializer = AFHTTPResponseSerializer()
-        manager.GET(testURL, parameters: nil,
-                    success: {(operation, responseObject) in
-                        let str : NSString = NSString(data:responseObject as! NSData, encoding:NSUTF8StringEncoding)!
+        
+        // GETメッセージを作成
+        let getStr = testGetURL + "?param1=hoge&param2=mage"
+        manager.GET(getStr, parameters: nil,
+                    success: {(operation, responsedObject) in
+                        let str : NSString = NSString(data:responsedObject as! NSData, encoding:NSUTF8StringEncoding)!
                         print("response: \(str)")
         },
             
@@ -125,6 +145,124 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
         )
     }
+    
+    // POSTで送信
+    // プレーンテキスト受信
+    func testAFNetworkingPOST() {
+        let manager = AFHTTPRequestOperationManager()
+        manager.responseSerializer = AFHTTPResponseSerializer()
+        let param:Dictionary<String, String> = ["param1" : "value1", "param2" : "value2"]
+        
+        manager.POST(testPostURL, parameters: param,
+                     success: {
+                        (operation: AFHTTPRequestOperation!, responsedObject:AnyObject!) in
+                        let str : NSString = NSString(data:responsedObject as! NSData, encoding:NSUTF8StringEncoding)!
+                        print("response: \(str)")
+
+            }, failure: {
+                (operation, error) in
+                print("Error: \(error)")
+        })
+    }
+    
+    // JSON受信
+    func testAFNetworkingJSON() {
+        //リクエスト
+        let manager = AFHTTPRequestOperationManager()
+        manager.requestSerializer = AFJSONRequestSerializer()
+        
+        manager.GET(testJsonURL, parameters: nil,
+                    success: {(operation: AFHTTPRequestOperation!, responsedObject: AnyObject!) in
+                        print("Success!!")
+                        
+                        // responsedObject を表示。ただ、Dictionary型なので存在しない要素にアクセスしようとしようとすると落ちる
+                        print(responsedObject)
+                        print(responsedObject["a"])
+                        print(responsedObject["b"])
+                        // print(responsedObject[0])        // 落ちる
+                        
+                        // SwiftyJSONオブジェクトに変換
+                        let json = JSON(responsedObject)
+                        print(json)
+                        print(json["a"])
+                        print(json["b"])
+                        print(json[0])           // 落ちない
+            },
+                    failure: {(operation, error) in
+                        print("Error!!")
+            }
+        )
+    }
+    
+    // テキスト形式JSON受信
+    func testAFNetworkingJSON2() {
+        //リクエスト
+        let manager = AFHTTPRequestOperationManager()
+        manager.responseSerializer = AFHTTPResponseSerializer()  // textを受け取る
+        
+        let param:Dictionary = ["param1" : "value1", "param2" : "value2",
+                                 "array1" : [1,2,3,4,5],
+                                 "dic1" : ["key1" : "123", "key2" : "hoge"]]
+        
+        manager.POST(testJsonURL2, parameters: param,
+                    success: {(operation: AFHTTPRequestOperation!, responsedObject: AnyObject!) in
+                        print("Success!!")
+                        let str : NSString = NSString(data:responsedObject as! NSData, encoding:NSUTF8StringEncoding)!
+                        print("response: \(str)")
+                        
+                        // print(responsedObject["param1"]);  responsedObjectはJSONオブジェクトになっていないのでエラー
+
+                        // SwiftyJSONオブジェクトに変換
+                        // phpが返すのは json_encode したJSON文字列
+                        if let response = responsedObject {
+                            let json = JSON(data:response as! NSData)
+                            print(json["param1"])
+                            print(json["param2"])
+                            print(json["array1"])
+                            print(json[0])           // 落ちない
+                        }
+            },
+                    failure: {(operation, error) in
+                        print("Error!!")
+            }
+        )
+    }
+    
+    // POSTしたデータをJSONオブジェクト形式で受信
+    func testAFNetworkingJSON3() {
+        //リクエスト
+        let manager = AFHTTPRequestOperationManager()
+        manager.requestSerializer = AFHTTPRequestSerializer()  // 文字列形式で送る
+        manager.responseSerializer = AFJSONResponseSerializer()  // JSONオブジェクトを変換して受け取る
+        
+        let param:Dictionary = ["param1" : "value1", "param2" : "value2",
+                                "array1" : [1,2,3,4,5],
+                                "dic1" : ["key1" : "123", "key2" : "hoge"]]
+        
+        manager.POST(testJsonURL3, parameters: param,
+                     success: {(operation: AFHTTPRequestOperation!, responsedObject: AnyObject!) in
+                        print("Success!!")
+                        
+                        print(responsedObject["param1"]);
+                        
+                        // SwiftyJSONオブジェクトに変換
+                        // phpが返すのは json_encode したJSON文字列
+                        if let response = responsedObject {
+                            let json = JSON(response)
+                            print(json["param1"])
+                            print(json["param2"])
+                            print(json["array1"])
+                            print(json["dic1"])
+                        }
+            },
+                     failure: {(operation, error) in
+                        print("Error!!")
+                        print(error)
+            }
+        )
+    }
+
+
     
     func initViews() {
         button1.addTarget(self, action: #selector(self.button1Tapped(_:)), forControlEvents: .TouchUpInside)
